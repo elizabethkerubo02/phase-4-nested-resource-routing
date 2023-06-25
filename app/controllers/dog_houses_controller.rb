c3d8d9aedce3609c593fd046528f4c5510facae0
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-06-25 17:21:15 +0300

    done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -3,8 +3,19 @@ class DogHousesController < ApplicationController
 
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    render json: dog_house, include: :reviews
   end
+  # def reviews_index
+  #   dog_house = DogHouse.find(params[:dog_house_id])
+  #   reviews = dog_house.reviews
+  #   render json: reviews, include: :dog_house
+  # end
+
+  # def review
+  #   review = Review.find(params[:id])
+  #   render json: review, include: :dog_house
+  # end
+  
 
   private
 
